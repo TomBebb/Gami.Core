0cf3c6fd263f78673b0709b76d3a2a6b137f04ab
Tom Bebbington <tombebb@protonmail.com>
2024-09-29 10:51:09 +0100

    Store metadata as plain arrays

--- Models/GameMetadata.cs
+++ Models/GameMetadata.cs
@@ -1,28 +1,29 @@
 ﻿using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
+// ReSharper disable UnusedMember.Global
 
 namespace Gami.Core.Models;
 
 [SuppressMessage("ReSharper", "PropertyCanBeMadeInitOnly.Global")]
 [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
-public record struct GameMetadata
+public record  GameMetadata
 {
-    public string? Description { get; set; }
+    public string Description { get; set; } = "";
 
-    public ImmutableArray<string>? Developers { get; set; }
-    public ImmutableArray<string>? Genres { get; set; }
-    public ImmutableArray<string>? Platforms { get; set; }
-    public ImmutableArray<string>? Publishers { get; set; }
-    public ImmutableArray<string>? Series { get; set; }
+    public ImmutableArray<string> Developers { get; set; } = ImmutableArray<string>.Empty;
+    public ImmutableArray<string> Genres { get; set; } = ImmutableArray<string>.Empty;
+    public ImmutableArray<string> Platforms { get; set; } = ImmutableArray<string>.Empty;
+    public ImmutableArray<string> Publishers { get; set; } = ImmutableArray<string>.Empty;
+    public ImmutableArray<string> Series { get; set; } = ImmutableArray<string>.Empty;
 
     public GameMetadata Combine(GameMetadata other) =>
         new()
         {
-            Description = Description ?? other.Description,
-            Developers = Developers ?? other.Developers,
-            Genres = Genres ?? other.Genres,
-            Platforms = Platforms ?? other.Platforms,
-            Publishers = Publishers ?? other.Publishers,
-            Series = Series ?? other.Series
+            Description = Description.Length == 0 ? other.Description: Description,
+            Developers = Developers.Concat(other.Developers).ToImmutableArray(),
+            Genres = Genres.Concat(other.Genres).ToImmutableArray(),
+            Platforms = Platforms.Concat(other.Platforms).ToImmutableArray(),
+            Publishers = Publishers.Concat(other.Publishers).ToImmutableArray(),
+            Series = Series.Concat(other.Series).ToImmutableArray()
         };
 }
