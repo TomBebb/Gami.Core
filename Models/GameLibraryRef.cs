d99e6259630ed14f96769bc7b4099eb065b5210b
Tom Bebbington <tombebb@protonmail.com>
2024-09-30 18:28:10 +0100

    Add tostring to GameLibraryRef.cs

--- Models/GameLibraryRef.cs
+++ Models/GameLibraryRef.cs
@@ -5,4 +5,6 @@ public class GameLibraryRef : IGameLibraryRef
     public string Name { get; set; } = null!;
     public string LibraryType { get; set; } = null!;
     public string LibraryId { get; set; } = null!;
+
+    public override string ToString() => $"{Name} (${LibraryType}: {LibraryId})";
 }
