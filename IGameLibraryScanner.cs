3c170c0ee25c06713538a5cd316fcc5936eef06d
Tom D Bebbington <tombebb@protonmail.com>
2024-11-26 20:35:40 +0000

    Revert "#2 skip IDs"
    This reverts commit 4857bdf396b6bb9221f52e1c90b52e3e397b2143.

--- IGameLibraryScanner.cs
+++ IGameLibraryScanner.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Frozen;
-using Gami.Core.Models;
+﻿using Gami.Core.Models;
 
 namespace Gami.Core;
 
 public interface IGameLibraryScanner : IBaseAddon
 {
-    public IAsyncEnumerable<IGameLibraryMetadata> Scan(FrozenSet<string> skipLibraryIds);
+    public IAsyncEnumerable<IGameLibraryMetadata> Scan();
 }
